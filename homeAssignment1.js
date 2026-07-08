38c0895eeca09bc37136fd1260b1636438165733
psm59204 <150443732+psm59204@users.noreply.github.com>
2026-07-08 08:47:00 +0530

    Add files via upload

---
+++ homeAssignment1.js
@@ -0,0 +1,15 @@
+let userName = "psm59204@gmail.com"
+let organization = "testleaf"
+let contactNumber = 9600097120
+let knowsAutomation = true
+let usesPlaywright
+
+console.log(userName + " -> " + "type is" + " " + typeof(userName) );
+console.log(organization + " -> " + "type is" + " " + typeof(organization));
+console.log(contactNumber + " -> " + "type is" + " " + typeof(contactNumber));
+console.log(knowsAutomation + " -> " + "type is" + " " + typeof(knowsAutomation));
+console.log(usesPlaywright + " -> " + "type is" + " " + typeof(usesPlaywright));
+
+
+
+
